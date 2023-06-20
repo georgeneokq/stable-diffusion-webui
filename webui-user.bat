9904d7e68cbdd3086c46df866629bb6a05eacfb7
George <george.neokq@gmail.com>
2023-06-20 21:01:19 +0800

    chore: add personal-env.bat to hold personal env variables

--- webui-user.bat
+++ webui-user.bat
@@ -5,4 +5,8 @@ set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 
+REM To prevent personal configuration from getting committed to git, define env variables in personal-env.bat
+REM For backward compatibility purposes, since everyone uses webui-user.bat to start the program.
+if exist personal-env.bat call personal-env.bat
+
 call webui.bat
